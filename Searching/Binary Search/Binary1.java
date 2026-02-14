6f405fb3a026fe015b9cda096e8824322a784d82
Shravani-14-in <shravanibhoir1408@gmail.com>
2026-02-14 15:28:41 +0530

    Add class definition and main method to Binary1.java

--- Searching/Binary Search/Binary1.java
+++ Searching/Binary Search/Binary1.java
@@ -1,2 +1,6 @@
 //Binary Search using Recursive Method
-
+class Binary1{
+  Public Static void main(String [] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter the number of elements: ");
+    
